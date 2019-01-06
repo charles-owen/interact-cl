a393a20bc743cba2e33e6d8824909745855771a3
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:03:40 -0500

    New cl-publish system.

---
+++ publish.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+	dist: path.resolve(__dirname, 'dist'),
+	files: [
+		'Interact\\.js'
+	]
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,7 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		Interact: path.resolve(__dirname, 'index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/interact.*'), destination: path.resolve(__dirname, 'dist') }
-				]
-			}
-		})
-	]
+	}
 }
