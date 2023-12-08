b2d7d5d4fb996f916550a3e8f30669c6c8c37d92
Dr. Charles B. Owen <cbowen@msu.edu>
2023-12-07 22:17:58 -0500

    Trying to fix the slow server issues.

--- js/Vue/InteractionPresenter.vue
+++ js/Vue/InteractionPresenter.vue
@@ -16,90 +16,99 @@
 
 
   export default {
-      props: ['selected', 'data'],
+    props: ['selected', 'data'],
     emits: ['reloaded', 'deleted', 'select'],
-      data: function() {
-          return {
-              root: this.$site.root,
-              interaction: null,
-              interactives: []
-          }
-      },
-      watch: {
-	      selected: function() {
-              this.fetch();
-              this.interactives = [];
-          }
-      },
-      components: {
-          interaction: InteractionVue,
-          discussions: DiscussionsVue
-      },
-      mounted() {
-          this.fetch();
-          this.$interact.presenter = this;
-      },
+    data: function () {
+      return {
+        root: this.$site.root,
+        interaction: null,
+        interactives: []
+      }
+    },
+    watch: {
+      selected: function () {
+        this.fetch();
+        this.interactives = [];
+      }
+    },
+    components: {
+      interaction: InteractionVue,
+      discussions: DiscussionsVue
+    },
+    mounted() {
+      this.fetch();
+      this.$interact.presenter = this;
+    },
     beforeUnmount() {
-      	  this.$interact.presenter = null;
-      },
-      methods: {
-          fetch() {
-              if(this.selected === 0) {
-	                this.interaction = null;
-                  return;
+      this.$interact.presenter = null;
+    },
+    methods: {
+      fetch() {
+        if (this.selected === 0) {
+          this.interaction = null;
+          return;
+        }
+
+        this.$site.api.get('/api/interact/interaction/' + this.selected, {})
+            .then((response) => {
+              if (!response.hasError()) {
+                this.take(response);
+              } else {
+                this.interaction = null;
+                this.$site.toast(this, response);
               }
 
-              this.$site.api.get('/api/interact/interaction/' + this.selected, {})
-                  .then((response) => {
-                      if (!response.hasError()) {
-                      	this.take(response);
-                      } else {
-                      	this.interaction = null;
-	                      this.$site.toast(this, response);
-                      }
+            })
+            .catch((error) => {
+              console.log('error')
+              this.interaction = null;
+              this.$site.toast(this, error);
+            });
+      },
+      take(response) {
+        const data = response.getData('interaction');
+        if (data !== null) {
+            this.interaction = new Interaction(response.getData('interaction').attributes)
+            this.$emit('reloaded', this.interaction)
+        }
+      },
+      deleted(interaction) {
+        this.$emit('deleted', interaction);
+      },
+      reloaded(interaction) {
+        this.interaction = interaction;
+        this.$emit('reloaded', interaction);
+      },
+      select(id) {
+        this.$emit('select', id);
+      },
+      prePolling(params) {
+        if (this.interaction !== null) {
+          let query = {id: this.selected, after: this.interaction.time};
+          params.interaction = query;
+        }
+      },
+      postPolling(response) {
+        // Only use the polling result if the current interaction
+        // has not changed. This can happen if the network is very
+        // slow where the results are after you changed to a new
+        // interaction.
+        const interactionData = response.getData('interaction')
+        if(this.interaction !== null && this.interaction.id === interactionData.attributes.id) {
+          this.take(response);
+        } else {
+          console.log('rejected polling result')
+        }
 
-                  })
-                  .catch((error) => {
-                    console.log('error')
-              	    this.interaction = null;
-	                  this.$site.toast(this, error);
-                  });
-          },
-          take(response) {
-          	const data = response.getData('interaction');
-          	if(data !== null) {
-	            this.interaction = new Interaction(response.getData('interaction').attributes);
-	            this.$emit('reloaded', this.interaction);
-            }
-          },
-          deleted(interaction) {
-          	  this.$emit('deleted', interaction);
-          },
-          reloaded(interaction) {
-          	this.interaction = interaction;
-          	this.$emit('reloaded', interaction);
-          },
-          select(id) {
-          	this.$emit('select', id);
-          },
-          prePolling(params) {
-            if(this.interaction !== null) {
-            	let query = {id: this.selected, after: this.interaction.time};
-	            params.interaction = query;
-            }
-          },
-          postPolling(response) {
-            this.take(response);
-            const data = response.getData('interactives');
-
-            if(data !== null) {
-            	this.interactives = data.attributes;
-            	//console.log(this.interactives);
-            } else {
-            	this.interactives = [];
-            }
-          }
+        const data = response.getData('interactives');
+        if (data !== null) {
+          this.interactives = data.attributes;
+          //console.log(this.interactives);
+        } else {
+          this.interactives = [];
+        }
       }
+    }
   }
 
 </script>
